00119823e5e3bf7e193b36850cb5972fcdf0cbdf
Jose Tomas <josetomascodecido@gmail.com>
2018-06-19 19:41:21 -0400

    exercise1

---
+++ ejercicio1.rb
@@ -0,0 +1,17 @@
+=begin
+Crear un método que reciba dos strings, este método creará un archivo index.html y pondrá como párrafo cada uno de los strings recibidos.
+- Crear un método similar al anterior, que además pueda recibir un arreglo. Si el arreglo no está vacío, agregar debajo de los párrafos una lista ordenada con cada uno de los elementos.
+- Crear un tercer método que además pueda recibir un color. Agregar color de fondo a los párrafos.
+- El retorno de los métodos debe devolver nil.
+=end
+
+
+def two_string(s1, s2)
+  File.open('index.html', 'w') { |file|
+    file.puts "<p>#{s1}"
+    file.puts "<p>#{s2}"
+  }
+end
+s1 = "Lorem ipsum dolor sit amet, consectetur "
+s2 = "Lorem ipsum dolor sit amet, consectetur "
+two_string(s1, s2)
