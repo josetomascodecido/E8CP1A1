1217bdc1e7d663a838f8c4660fa68676d14abb2e
Jose Tomas <josetomascodecido@gmail.com>
2018-06-19 21:05:59 -0400

    exercise1 done

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -8,26 +8,49 @@ Crear un método que reciba dos strings, este método creará un archivo index.h
 
 def two_string(s1, s2)
   File.open('index.html', 'w') { |file|
-    file.puts "<p>#{s1}"
-    file.puts "<p>#{s2}"
+    file.puts "<p>#{s1}</p>"
+    file.puts "<p>#{s2}</p>"
   }
 end
+
 s1 = "Lorem ipsum dolor sit amet, consectetur "
 s2 = "Lorem ipsum dolor sit amet, consectetur "
-s3 = ["hola", "esta", "es", "mi", "prueba"]
-
-
+s3 = []
+s4 = "<head>
+        <style>
+          p{
+            background-color: lightblue;
+           }
+        </style>
+      </head>"
 two_string(s1, s2)
+
 
 def tres_string(s1, s2, s3)
   File.open('index.html', 'w') do |file|
-    file.puts "<p>#{s1}"
-    file.puts "<p>#{s2}"
+    file.puts "<p>#{s1}</p>"
+    file.puts "<p>#{s2}</p>"
+    if ! s3.empty?
+      file.puts "<ol>"
+    s3.each{|x|file.puts "<li>#{x}</li>"}
+    file.puts "</ol>"
+    end
+
+  end
+end
+tres_string(s1, s2, s3)
+
+def cuatro(s1, s2, s3, s4)
+  File.open('index.html', 'w') do |file|
+    file.puts "<p>#{s1}</p>"
+    file.puts "<p>#{s2}</p>"
     file.puts "<ol>"
     if ! s3.empty?
     s3.each{|x|file.puts "<li>#{x}</li>"}
     end
     file.puts "</ol>"
+    file.puts s4
   end
 end
-tres_string(s1, s2, s3)
+cuatro(s1, s2, s3, s4)
+puts 
