6726b5579968fd4b7f91c0a1de3336e57b555bfd
Jose Tomas <josetomascodecido@gmail.com>
2018-06-19 20:02:42 -0400

    exercise1

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -14,4 +14,16 @@ def two_string(s1, s2)
 end
 s1 = "Lorem ipsum dolor sit amet, consectetur "
 s2 = "Lorem ipsum dolor sit amet, consectetur "
+s3 = ["hola", "esta", "es", "mi", "prueba"]
+
+
 two_string(s1, s2)
+
+def tres_string(s1, s2, s3)
+  File.open('index.html', 'w') { |file|
+    file.puts "<p>#{s1}"
+    file.puts "<p>#{s2}"
+    s3.each{|x|file.puts "<ol>#{x}</ol>"}
+  }
+end
+tres_string(s1, s2, s3)
